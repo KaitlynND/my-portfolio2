3f7e4db7b02a18872b15f5ceb87f3ced443b184a
Kaitlyn <cyberk1019@live.com>
2020-06-18 01:13:17 -0500

    End of week 2

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,117 @@
 
 package com.google.sps.servlets;
 
+//import com.google.sps.data.ServerStats;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+//import com.google.sps.data.Task;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Scanner;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+   private final ArrayList<String> jstring = new ArrayList<String>();
+   // private final ArrayList<String> tasks = new ArrayList<String>();
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+
+    
+@Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String text = getParameter(request, "text-input", "");
+    response.sendRedirect("/index.html");
+
+    String title = request.getParameter("title");
+
+    Entity taskEntity = new Entity("Task");
+    taskEntity.setProperty("title", title);
+    
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(taskEntity);
+
+    response.sendRedirect("/index.html");
+
+
+
+  }   
+
+    
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+        String value = request.getParameter(name);
+            if (value == null) {
+                return defaultValue;
+                }
+                return value;
+                 }
+
+
+
+
+   
+   
+    /**
+   * @return 
+   */
+ 
+    
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+ Entity employee = new Entity("Employee", "asalieri");
+ employee.setProperty("firstName", "Antonio");
+ employee.setProperty("lastName", "Salieri");
+// employee.setProperty("hireDate", new Date());
+ employee.setProperty("attendedHrTraining", true);
+
+ DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+ datastore.put(employee);
+ 
+    
+
+        
+response.setContentType("text/html;");
+response.getWriter().println("Hello Kaitlyn!");
 }
+}
+
+
+
+    
+
+
+
+
+
+
+
+  
+ /* private String convertToJsonUsingGson(ArrayList<String> jstring) {
+      Gson gson = new Gson();
+      String json = gson.toJson(jstring);
+      return json;*/
+
+        /* jstring.add("Viola");
+      jstring.add("Trumpet");
+      jstring.add("Cello");
+
+
+    response.getWriter().println(jstring);*/
+  
+
+
+
+
+  
+  
+
+
